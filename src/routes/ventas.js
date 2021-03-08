6dbaa28d2b887f510a63e02a15d27553cd025f00
unknown <marcoscastillo871@gmail.com>
2021-03-07 19:21:17 -0600

    modificación scroll

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -1,101 +1,93 @@
 const express = require('express');
-const { route } = require('./links');
 const router = express.Router();
-
 const pool = require('../database');
-const {isLoggedIn} = require('../lib/auth');
+const { isLoggedIn } = require('../lib/auth');
 
-// const a = {title,url} = require('./links');
-// console.log(a);
-// // router.get('/venta', isLoggedIn, (req,res)=>{
-// //     res.render('ventas/venta');
-// // });
+router.get('/factura', isLoggedIn, async (req, res) => {
+    res.render('ventas/factura');
+})
 
-// // router.post('/venta', isLoggedIn, async (req, res)=>{
-// //     const { title, url, cantidad, description} = req.body;
-    
-// //     const newLink= {
-// //         title,
-// //         url,
-// //         cantidad,
-// //         description,
-// //         user_id: req.user.id
-// //     };
-// //     await pool.query('INSERT INTO ventas set ?', [newLink]);
-// //     console.log(newLink);
-// //     req.flash('success', 'guardado correctamente');
-// //     res.redirect('/ventas');
-// // });
+router.post('/factura', isLoggedIn, async (req, res) => {
+    let { cliente, nit, total, detalle } = req.body;
 
-router.get('/venta', isLoggedIn, (req,res)=>{
-    res.render('ventas/venta');
-});
+    if(detalle.length===0){
+        req.flash("Datos vacios");
+        res.json("false");
+    }
 
 
-router.post('/venta', isLoggedIn, async (req, res)=>{
-   
-    const { cliente, cantidad, description} = req.body;
-    
-    const newLink= {
+    const ventas = {
+        user_id: req.user.id,
         cliente,
-        cantidad,
-        description,
-        user_id: req.user.id
-    };
-    await pool.query('INSERT INTO ventas1 set ?', [newLink]);
-    // console.log(newLink);
-  
+        nit,
+        total
+    }
+
+    let respuesta = await pool.query('Insert INTO ventas set ?', ventas);
+
+    await quitarProducto(detalle, respuesta.insertId);
+
+
+    console.log("FINAL :V");
+
     req.flash('success', 'guardado correctamente');
+    res.json('ok');
+})
+
+let quitarProducto = (detalle, idVenta) => {
+
+    const promesas = detalle.map((element) => {
+        return new Promise(async (resolve) => {
+            let { id: id_producto } = element;
+
+            delete element.id;
+            element.venta_id = idVenta;
+
+            let insertado = await pool.query('Insert into detalle set ?', element);
+
+            console.log("insertado:", insertado);
+
+            let producto = await pool.query('Select * from producto where id=?', id_producto);
+
+            if (producto.length > 0) {
+                producto[0].unidades -= element.cantidad;
+                let actualizado = await pool.query('UPDATE producto set unidades=? where id=?', [producto[0].unidades, id_producto]);
+                resolve("actualizado");
+            }
+        })
+    })
+
+    return new Promise(resolve => {
+        Promise.all(promesas).then(() => {
+            resolve("TErminado");
+        })
+    })
+}
+
+router.get('/productos', isLoggedIn, async (req, res) => {
+    const productos = await pool.query('SELECT id, title, unidades, precio_venta,description FROM producto WHERE unidades>0');
+    res.json(productos);
+})
+
+router.get('/', isLoggedIn, async (req, res) => {
+    // const ventas = await pool.query('select * from ventas order by created_at desc');
+    const ventas = await pool.query('select * from ventas where MONTH(created_at)=MONTH(CURDATE()) order by created_at desc');
+    const response= await pool.query('select sum(total) as total from ventas where MONTH(created_at)=MONTH(CURDATE()) ');
+
+    // res.render('ventas/ListVentas', { ventas,'total': response[0].total});
+    res.render('ventas/ListVentas', { ventas,'total': response[0].total.toFixed(2)});
+});
+
+router.get('/:id',isLoggedIn, async(req,res)=>{
+    const detalle= await pool.query('Select * from detalle where venta_id=?',req.params.id);
+    res.render('ventas/detalle',{detalle});
+});
+
+router.get('/delete/:id',isLoggedIn, async(req,res)=>{
+    await pool.query('Delete from detalle where venta_id=?',req.params.id);
+   await pool.query('Delete from ventas where id=?',req.params.id);
+
     res.redirect('/ventas');
-});
+})
 
-// router.get('/RVentas', isLoggedIn, async(req, res)=>{
-//     const links = await pool.query('SELECT * FROM ventas WHERE user_id = ?', [req.user.id]);
-//     console.log(links);
-//     res.render('ventas/ListVentas', {links});
-// });
-
-var prueba = '';
-router.get('/', isLoggedIn, async(req, res)=>{
-    const{cliente}= req.params;
-    // const li= {
-    //     title,
-    //     url,
-    //     user_id: req.user.id
-    // }
-    const ventas = await pool.query('SELECT * FROM ventas1 WHERE user_id = ?', [req.user.id]);
-    // const links = await pool.query('SELECT * FROM links WHERE user_id = ?', [req.user.id]);
-    const links = await pool.query('SELECT * FROM links');
-    // console.log(ventas);
-    // prueba = links[0];
-     const unir = {...ventas, ...links};
-    // const b= Object.assign(ventas,links);
-    // ventas[1][1] =links[0];
-   
-      console.log(unir);
-    res.render('ventas/ListVentas', {links, ventas});
-});
-
-// // router.get('/allLinks', isLoggedIn, async(req, res)=>{
-// //     const links = await pool.query('SELECT * FROM links');
-// //     console.log(links);
-// //     res.render('links/list', {links});
-// // });
-
-// // router.get('/delete/:id', isLoggedIn, async(req, res) =>{
-// //     const {id} = req.params;
-// //  await pool.query('DELETE FROM links WHERE ID = ?', [id]);
-// //  req.flash('success', 'Se ha eliminado correctamente');
-// // res.redirect('/links');
-
-// });
-// var prueba = '';
-// router.get('/edit/:id', isLoggedIn, async (req, res) =>{
-//     const {id} = req.params;
-//     const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
-//    console.log(links[0]);
-//    prueba = links[0].title;
-//    res.render('links/edit', {link: links[0]});                                                
-// });
-
-module.exports = router;
+module.exports = router
