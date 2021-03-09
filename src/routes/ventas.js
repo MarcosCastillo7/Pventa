730d12a131ea89271f2cfe8c070795ceae54fd2b
CarloEz <marcarlos47@gmail.com>
2021-03-08 19:51:42 -0600

    version 2
    factura:algunos errores, listventas: Busqueda por mes, Productos:busqueda

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -69,15 +69,53 @@ router.get('/productos', isLoggedIn, async (req, res) => {
 })
 
 router.get('/', isLoggedIn, async (req, res) => {
-    const ventas = await pool.query('select * from ventas where MONTH(created_at)=MONTH(CURDATE()) order by created_at desc');
-    const response= await pool.query('select sum(total) as total from ventas');
-
-    res.render('ventas/ListVentas', { ventas,'total': response[0].total.toFixed(2)});
+    let mes=await ventasMes(null);
+    res.render('ventas/ListVentas', { 'ventas':mes.ventas,'total': mes.total, 'fecha':mes.date});
 });
+
+router.post('/',isLoggedIn,async(req,res)=>{
+    let {date}= req.body;
+    
+    objeto={
+        mes: parseInt(date.slice(-2)),
+        anio: parseInt(date.slice(0, 4))
+    }
+    let mes= await ventasMes(objeto);
+    console.log(mes)
+    res.render('ventas/ListVentas',{'ventas':mes.ventas,'total': mes.total, 'fecha':mes.date})
+})
 
 router.get('/:id',isLoggedIn, async(req,res)=>{
     const detalle= await pool.query('Select * from detalle where venta_id=?',req.params.id);
     res.render('ventas/detalle',{detalle});
 })
+
+
+let ventasMes=async(date=null)=>{
+    
+    if(date===null){    
+        var f = new Date();
+        console.log(f)
+        date={
+            mes:f.getMonth()+1,
+            anio:f.getFullYear()
+        }
+    }   
+
+    const ventas = await pool.query(`select * from ventas where MONTH(created_at)=${date.mes} and YEAR(created_at)=${date.anio}  order by created_at desc`);
+    const response= await pool.query(`select sum(total) as total from ventas where MONTH(created_at)= ${date.mes} and YEAR(created_at)= ${date.anio} order by total`);
+
+    if(response[0].total===null){
+        response[0].total=0;
+    }
+
+    totalMes={
+        ventas,
+        total:response[0].total.toFixed(2),
+        date: date.anio+'-'+('0'+date.mes).slice(-2)
+    }
+
+    return totalMes
+}
 
 module.exports = router
