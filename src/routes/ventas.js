95b3892fbc0443e4b8bff2633b75addecdeb9bf0
CarloEz <marcarlos47@gmail.com>
2021-03-11 20:24:07 -0600

    ventas.js y factura.hbs

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -10,10 +10,7 @@ router.get('/factura', isLoggedIn, async (req, res) => {
 router.post('/factura', isLoggedIn, async (req, res) => {
     let { cliente, nit, total, detalle } = req.body;
 
-    if(detalle.length===0){
-        req.flash("Datos vacios");
-        res.json("false");
-    }
+    console.log(detalle);
 
     const ventas = {
         user_id: req.user.id,
@@ -26,7 +23,6 @@ router.post('/factura', isLoggedIn, async (req, res) => {
 
     await quitarProducto(detalle, respuesta.insertId);
 
-
     console.log("FINAL :V");
 
     req.flash('success', 'guardado correctamente');
@@ -37,7 +33,7 @@ let quitarProducto = (detalle, idVenta) => {
 
     const promesas = detalle.map((element) => {
         return new Promise(async (resolve) => {
-            let { id: id_producto } = element;
+            let { id_producto } = element;
 
             delete element.id;
             element.venta_id = idVenta;
@@ -69,53 +65,77 @@ router.get('/productos', isLoggedIn, async (req, res) => {
 })
 
 router.get('/', isLoggedIn, async (req, res) => {
-    let mes=await ventasMes(null);
-    res.render('ventas/ListVentas', { 'ventas':mes.ventas,'total': mes.total, 'fecha':mes.date});
-});
+    let mes = await ventasMes(null);
+    res.render('ventas/ListVentas', { 'ventas': mes.ventas, 'total': mes.total, 'fecha': mes.date });
+})
 
-router.post('/',isLoggedIn,async(req,res)=>{
-    let {date}= req.body;
-    
-    objeto={
+router.post('/', isLoggedIn, async (req, res) => {
+    let { date } = req.body;
+
+    objeto = {
         mes: parseInt(date.slice(-2)),
         anio: parseInt(date.slice(0, 4))
     }
-    let mes= await ventasMes(objeto);
-    console.log(mes)
-    res.render('ventas/ListVentas',{'ventas':mes.ventas,'total': mes.total, 'fecha':mes.date})
+    let mes = await ventasMes(objeto);
+
+    res.render('ventas/ListVentas', { 'ventas': mes.ventas, 'total': mes.total, 'fecha': mes.date })
 })
 
-router.get('/:id',isLoggedIn, async(req,res)=>{
-    const detalle= await pool.query('Select * from detalle where venta_id=?',req.params.id);
-    res.render('ventas/detalle',{detalle});
+router.get('/:id', isLoggedIn, async (req, res) => {
+    const detalle = await detalleVenta(req.params.id);
+    res.render('ventas/detalle', { detalle });
 })
 
+let ventasMes = async (date = null) => {
 
-let ventasMes=async(date=null)=>{
-    
-    if(date===null){    
+    if (date === null) {
         var f = new Date();
         console.log(f)
-        date={
-            mes:f.getMonth()+1,
-            anio:f.getFullYear()
+        date = {
+            mes: f.getMonth() + 1,
+            anio: f.getFullYear()
         }
-    }   
-
-    const ventas = await pool.query(`select * from ventas where MONTH(created_at)=${date.mes} and YEAR(created_at)=${date.anio}  order by created_at desc`);
-    const response= await pool.query(`select sum(total) as total from ventas where MONTH(created_at)= ${date.mes} and YEAR(created_at)= ${date.anio} order by total`);
-
-    if(response[0].total===null){
-        response[0].total=0;
     }
 
-    totalMes={
+    const ventas = await pool.query(`select * from ventas where MONTH(created_at)=${date.mes} and YEAR(created_at)=${date.anio}  order by created_at desc`);
+    const response = await pool.query(`select sum(total) as total from ventas where MONTH(created_at)= ${date.mes} and YEAR(created_at)= ${date.anio} order by total`);
+
+    if (response[0].total === null) {
+        response[0].total = 0;
+    }
+
+    totalMes = {
         ventas,
-        total:response[0].total.toFixed(2),
-        date: date.anio+'-'+('0'+date.mes).slice(-2)
+        total: response[0].total.toFixed(2),
+        date: date.anio + '-' + ('0' + date.mes).slice(-2)
     }
 
     return totalMes
 }
+
+let detalleVenta = async (id) => {
+    return await pool.query('Select * from detalle where venta_id=?', id);
+}
+
+
+//EN proceso :V
+/* router.get('/imprimir/listado/:date', async (req, res) => {
+    const date = req.params.date;
+
+    objeto = {
+        mes: parseInt(date.slice(-2)),
+        anio: parseInt(date.slice(0, 4))
+    }
+    let mes = await ventasMes(objeto);
+
+    for (let element of mes.ventas) {
+        element.detalle = await detalleVenta(element.id);
+    }
+
+
+    
+    res.render('ventas/imprimirlistado', mes);
+}) */
+
 
 module.exports = router
